dc3453db13eb786001ec007c435e305f2b6b2dde
Дмитро Миронов <hattabith@gmail.com>
2023-09-19 13:29:09 +0300

    Create solution

---
+++ PartialDischargeMeasurementApp/FileParser.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+public class FileParser
+{
+    private string _fileName;
+    public FileParser(string file)
+    {
+        _fileName = file;
+    }
+    
+}
---
+++ PartialDischargeMeasurementApp/Program.cs
@@ -0,0 +1,12 @@
+﻿string? fileName = null;
+if (args.Length == 0)
+{
+    Console.WriteLine("No arguments passed");
+    fileName = "default.txt";
+}
+if (args.Length > 0)
+{
+    fileName = args[0];
+}
+
+Console.WriteLine("File name is: {0}", fileName);
